b2b05ae67fc4740fcd6ab725cc43ef08b232a9f2
Philippe Lafrance <phil.laf@gmail.com>
2015-05-27 02:53:21 -0400

    easily configurable database config added

--- config/app.default.php
+++ config/app.default.php
@@ -1,4 +1,45 @@
 <?php
+$default = json_decode(file_get_contents('gintonic.database.json'),true);
+if(!$default){
+    $default = [
+        'className' => 'Cake\Database\Connection',
+        'driver' => 'Cake\Database\Driver\Mysql',
+        'persistent' => false,
+        'host' => 'localhost',
+        /**
+         * CakePHP will use the default DB port based on the driver selected
+         * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
+         * the following line and set the port accordingly
+         */
+        //'port' => 'nonstandard_port_number',
+        'username' => 'my_app',
+        'password' => 'secret',
+        'database' => 'my_app',
+        'encoding' => 'utf8',
+        'timezone' => 'UTC',
+        'cacheMetadata' => true,
+
+        /**
+         * Set identifier quoting to true if you are using reserved words or
+         * special characters in your table or column names. Enabling this
+         * setting will result in queries built using the Query Builder having
+         * identifiers quoted when creating SQL. It should be noted that this
+         * decreases performance because each query needs to be traversed and
+         * manipulated before being executed.
+         */
+        'quoteIdentifiers' => false,
+
+        /**
+         * During development, if using MySQL < 5.6, uncommenting the
+         * following line could boost the speed at which schema metadata is
+         * fetched from the database. It can also be set directly with the
+         * mysql configuration directive 'innodb_stats_on_metadata = 0'
+         * which is the recommended value in production environments
+         */
+        //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
+    ];
+}
+
 return [
     /**
      * Debug Level:
@@ -208,43 +249,7 @@ return [
      * See vendor\cakephp\cakephp\src\Database\Driver for complete list
      */
     'Datasources' => [
-        'default' => [
-            'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Mysql',
-            'persistent' => false,
-            'host' => 'localhost',
-            /**
-             * CakePHP will use the default DB port based on the driver selected
-             * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
-             * the following line and set the port accordingly
-             */
-            //'port' => 'nonstandard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
-            'encoding' => 'utf8',
-            'timezone' => 'UTC',
-            'cacheMetadata' => true,
-
-            /**
-             * Set identifier quoting to true if you are using reserved words or
-             * special characters in your table or column names. Enabling this
-             * setting will result in queries built using the Query Builder having
-             * identifiers quoted when creating SQL. It should be noted that this
-             * decreases performance because each query needs to be traversed and
-             * manipulated before being executed.
-             */
-            'quoteIdentifiers' => false,
-
-            /**
-             * During development, if using MySQL < 5.6, uncommenting the
-             * following line could boost the speed at which schema metadata is
-             * fetched from the database. It can also be set directly with the
-             * mysql configuration directive 'innodb_stats_on_metadata = 0'
-             * which is the recommended value in production environments
-             */
-            //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
-        ],
+        'default' => $default, 
 
         /**
          * The test connection is used during the test suite.
